892a8197e7e307ee89d2e5e31d21e230a7cad24f
d-madiou <madioudiallo846@gmail.com>
2025-06-29 11:56:55 +0800

    The new one1

--- crm/models.py
+++ crm/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class customer(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
 
-class product(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveBigIntegerField(default=0)
     
-class order(models.Model):
-    customer = models.ForeignKey(customer, on_delete=models.CASCADE)
-    products = models.ManyToManyField(product)
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     order_date = models.DateTimeField(auto_now_add=True)
